d0abfe054920bb173664d76cc9dff49bc6f79f2f
Gauri Raut <95969496+curiousMind1234@users.noreply.github.com>
2026-09-22 23:25:46 +0530

    Create code.cpp

---
+++ Arrays/Second largest element/code.cpp
@@ -0,0 +1,19 @@
+int secondLargest(vector<int>& arr) {
+    int largest = INT_MIN;
+    int second = INT_MIN;
+
+    for (int x : arr) {
+        if (x > largest) {
+            second = largest;
+            largest = x;
+        }
+        else if (x > second && x != largest) {
+            second = x;
+        }
+    }
+
+    return second;
+}
+
+TC: O(N)
+SC: O(1)
